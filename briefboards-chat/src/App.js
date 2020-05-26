cc79f9ae214c5a0260f9e9f531f588099820a758
ezequielaranda81 <ezequiel.aranda@gmail.com>
2020-05-26 14:29:50 -0300

    example from Stream added

--- briefboards-chat/src/App.js
+++ briefboards-chat/src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Chat, Channel, ChannelList, Window } from 'stream-chat-react';
+import { ChannelHeader, MessageList } from 'stream-chat-react';
+import { MessageInput, Thread } from 'stream-chat-react';
+import { StreamChat } from 'stream-chat';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+import 'stream-chat-react/dist/css/index.css';
+
+const chatClient = new StreamChat('ujydutkcczb8');
+const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoicHVycGxlLW1lYWRvdy02In0.Hzkpjsva-5muqVamAIzRZhs6w4S9BNkiZV5zc3ZHVbg';
+
+// custom channel preview component
+class MyChannelPreview extends React.Component {
+  render() {
+      const {setActiveChannel, channel} = this.props;
+      const unreadCount = channel.countUnread();
+
+      return (
+      <div className="channel_preview">
+        <a href="#" onClick={(e) => setActiveChannel(channel, e)}>
+          {channel.data.name}
         </a>
-      </header>
-    </div>
-  );
+
+        <span>
+          Unread messages: {unreadCount}
+        </span>
+      </div>
+    );
+  }
 }
+
+// a very minimalistic message component
+class MyMessageComponent extends React.Component {
+  render() {
+    return <div><b>{this.props.message.user.name}</b> {this.props.message.text}</div>;
+  }
+}
+
+chatClient.setUser(
+  {
+    id: 'purple-meadow-6',
+    name: 'Purple meadow',
+    image: 'https://getstream.io/random_svg/?id=purple-meadow-6&name=Purple+meadow'
+  },
+  userToken,
+);
+
+const filters = { type: 'messaging', members: { $in: ['purple-meadow-6'] } };
+const sort = { last_message_at: -1 };
+const channels = chatClient.queryChannels(filters, sort);
+
+const App = () => (
+  <Chat client={chatClient} theme={'messaging light'}>
+    <ChannelList
+      filters={filters}
+      sort={sort}
+      Preview={MyChannelPreview}
+    />
+    <Channel Message={MyMessageComponent}>
+      <Window>
+        <ChannelHeader />
+        <MessageList />
+        <MessageInput />
+      </Window>
+      <Thread />
+    </Channel>
+  </Chat>
+);
 
 export default App;
